504484113d27aa5c143551afe3cee4fdf17ff98d
Jaytrin <jaytrin.apinchapong@gmail.com>
2018-10-04 18:43:20 -0700

    corrected syntax

--- webhook.php
+++ webhook.php
@@ -2,10 +2,9 @@
 
 require_once('credentials.php');
 //Log for errors
-error_log("something wrong, ".print_r($_POST,true)); This will show up in the terminal.
+// error_log("something wrong, ".print_r($_POST,true));
 
-
-$query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['email']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
+$query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['html']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
 
 mysqli_query($conn, $query);
 
