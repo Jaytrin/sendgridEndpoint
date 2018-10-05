968f4d330fb6d367bda39638b4efdefef6a45eac
Jaytrin <jaytrin.apinchapong@gmail.com>
2018-10-04 18:37:26 -0700

    added errorlog

--- webhook.php
+++ webhook.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once('credentials.php');
+//Log for errors
+error_log("something wrong, ".print_r($_POST,true)); This will show up in the terminal.
+
 
 $query = "INSERT INTO receivedEmail SET subject='{$_POST['subject']}', body='{$_POST['email']}', sender='{$_POST['from']}', recipient='{$_POST['to']}', sent=NOW()";
 
